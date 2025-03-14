58952ce9b1c04b2fe4e0c3c6e0b2afa14d98815b
shakeeb <shakee.zats@gmail.com>
2025-03-14 15:00:49 +0530

    update build files

--- includes/admin/page-optimizer/dist-debug/asset-map.php
+++ includes/admin/page-optimizer/dist-debug/asset-map.php
@@ -3,12 +3,12 @@
                         exit;
                     }
                     define('RAPIDLOAD_ASSET_MAP', [
-                        'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
-    'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
-    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
-    'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
-    'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
+                        'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
+    'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
+    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
+    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
+    'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/index.css' => 'assets/index.CTyKUOzS.css',
     'assets/index.js' => 'assets/index.DzRlbddd.js',
@@ -18,8 +18,8 @@
     'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
     'assets/index.Dv7C__IT.map' => 'assets/index.Dv7C__IT.js.map',
     'assets/InitTour.DepQiatX.map' => 'assets/InitTour.DepQiatX.js.map',
-    'assets/index.Be5yF97N.map' => 'assets/index.Be5yF97N.js.map',
     'assets/animations.CVpoelRy.map' => 'assets/animations.CVpoelRy.js.map',
+    'assets/index.Be5yF97N.map' => 'assets/index.Be5yF97N.js.map',
     'assets/core-vendor.V80Ff32-.map' => 'assets/core-vendor.V80Ff32-.js.map',
     'assets/vendor.T3CYoFTJ.map' => 'assets/vendor.T3CYoFTJ.js.map',
     'assets/index.DzRlbddd.map' => 'assets/index.DzRlbddd.js.map'
