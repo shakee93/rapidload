62ec7ac07b0a8898b47d0b5f4583a1f8703251bb
shakee93 <shakee.zats@gmail.com>
2025-04-04 15:31:34 +0530

    fix: phpcs for job status url

--- includes/RapidLoad_DB.php
+++ includes/RapidLoad_DB.php
@@ -696,8 +696,8 @@ abstract class RapidLoad_DB
         return true;
     }
 
-    static function updateRuleJobDataStatusWhere($status = 'queued', $where = "", $ids = []){
-
+    static function updateRuleJobDataStatusWhere($status = 'queued', $where = "", $ids = [], $whereAndStatus = null)
+    {
         global $wpdb;
 
         if (!empty($ids)) {
@@ -707,14 +707,33 @@ abstract class RapidLoad_DB
             $ids = implode(',', array_map('intval', $ids));
         }
 
-        $wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->prefix}rapidload_job_data SET status = %s WHERE job_id IN (%5s) {$where}", $status, $ids));
-
-        $error = $wpdb->last_error;
-
-        if(!empty($error)){
-            self::show_db_error($error);
+        if (empty($whereAndStatus)) {
+            $wpdb->query(
+                $wpdb->prepare(
+                    "UPDATE {$wpdb->prefix}rapidload_job_data SET status = %s WHERE job_id IN (%5s) %5s",
+                    $status,
+                    $ids,
+                    $where
+                )
+            );
+        } else {
+            $wpdb->query(
+                $wpdb->prepare(
+                    "UPDATE {$wpdb->prefix}rapidload_job_data SET status = %s WHERE job_id IN (%5s) AND status = %s %s",
+                    $status,
+                    $ids,
+                    $whereAndStatus,
+                    $where
+                )
+            );
         }
 
+        error_log($wpdb->last_query);
+
+        $error = $wpdb->last_error;
+        if (!empty($error)) {
+            self::show_db_error($error);
+        }
         return true;
     }
 
