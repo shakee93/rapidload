cd87d3d21294db5e4d672fd66455163919b29511
Shakeel <shakeel@freshpixl.com>
2025-04-09 17:01:21 +0530

    build

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -11,10 +11,10 @@
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.CaFmK9OX.css',
-    'assets/animations.js' => 'assets/animations.CVpoelRy.js',
-    'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
-    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
     'assets/index.js' => 'assets/index.DX-JEx5u.js',
-    'assets/InitTour.js' => 'assets/InitTour.BvD4NRRt.js'
+    'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
+    'assets/InitTour.js' => 'assets/InitTour.BvD4NRRt.js',
+    'assets/animations.js' => 'assets/animations.CVpoelRy.js',
+    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js'
                     ]);
                     
