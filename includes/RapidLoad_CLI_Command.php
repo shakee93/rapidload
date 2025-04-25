64dcf49b0568021f8ee0a14383c0961c3f444de1
shakeeb <shakee.zats@gmail.com>
2025-04-25 17:20:17 +0530

    RapidLoad_CLI_Command checked

--- includes/RapidLoad_CLI_Command.php
+++ includes/RapidLoad_CLI_Command.php
@@ -5,6 +5,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 include_once 'RapidLoad_Utils.php';
 
+if(class_exists('RapidLoad_CLI_Command')){
+    return;
+}
+
+/**
+ * Class RapidLoad_CLI_Command
+ */
+
 if (defined('WP_CLI') && WP_CLI) {
 
     class RapidLoad_CLI_Command
@@ -28,7 +36,7 @@ if (defined('WP_CLI') && WP_CLI) {
 
             $results           = $uucss_api->rapidload_api_post( 'connect', [ 'url' => $url, 'type' => 'wordpress' ] );
 
-            if ( $uucss_api->is_error( $results ) ) {
+            if ( $uucss_api->rapidload_api_is_error( $results ) ) {
                 if(isset($results->errors) && isset($results->errors[0])){
                     WP_CLI::error("License Key," . $results->errors[0]->detail . "! - " . get_site_url() . ' - ' . home_url() . ' - ' . $url . ' - ' . $message);
                 }else{
@@ -36,27 +44,27 @@ if (defined('WP_CLI') && WP_CLI) {
                 }
             }
 
-            $options = RapidLoad_Base::get_option( 'autoptimize_uucss_settings' , RapidLoad_Base::get_default_options());
+            $options = RapidLoad_Base::rapidload_get_option( 'autoptimize_uucss_settings' , RapidLoad_Base::rapidload_get_default_options());
             $options['uucss_api_key_verified'] = "1";
             $options['uucss_api_key']          = $license_key;
 
             if (isset($assoc_args['uucss'])) {
-                RapidLoad_Base::update_option('rapidload_module_css',"1");
+                RapidLoad_Base::rapidload_update_option('rapidload_module_css',"1");
                 $options['uucss_enable_uucss'] = "1";
             }
 
             if (isset($assoc_args['cpcss'])) {
-                RapidLoad_Base::update_option('rapidload_module_css',"1");
+                RapidLoad_Base::rapidload_update_option('rapidload_module_css',"1");
                 $options['uucss_enable_cpcss'] = "1";
             }
 
-            RapidLoad_Base::update_option( 'autoptimize_uucss_settings', $options );
+            RapidLoad_Base::rapidload_update_option( 'autoptimize_uucss_settings', $options );
 
             WP_CLI::success("License Key connected , $license_key!");
         }
 
         public function update_db(){
-            RapidLoad_DB::update_db();
+            RapidLoad_DB::rapidload_db_update_db();
             WP_CLI::success("Database updated");
         }
 
