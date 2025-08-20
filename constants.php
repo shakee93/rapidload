dd6bc8d45b16fd43a262a07b518e295b0fa7786c
azeezs2012 <azeezs2012@gmail.com>
2025-08-20 12:41:37 +0530

    chore: update version and changelog

--- constants.php
+++ constants.php
@@ -25,7 +25,7 @@ if ( ! defined( 'RAPIDLOAD_INDEX_FILE' ) ) {
 }
 
 if ( ! defined( 'RAPIDLOAD_VERSION' ) ) {
-    define( 'RAPIDLOAD_VERSION', '3.1.16' );
+    define( 'RAPIDLOAD_VERSION', '3.1.17' );
 }
 
 if ( ! defined( 'RAPIDLOAD_CACHE_CHILD_DIR' ) ) {
